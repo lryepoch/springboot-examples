756abefc3a5e4f25a00eb8d7bd6e485668572baa
lryepoch <1783510791@qq.com>
2020-12-08 18:54:33 +0800

    2020-12-08

---
+++ springboot-es/src/main/java/com/es/ElasticsearchApplication.java
@@ -0,0 +1,16 @@
+package com.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lryepoch
+ * @date 2020/12/7 9:00
+ * @description TODO es demo
+ */
+@SpringBootApplication
+public class ElasticsearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class, args);
+    }
+}
