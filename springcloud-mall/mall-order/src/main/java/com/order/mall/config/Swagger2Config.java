f3f43778618b7d56c61dbaef9d6897a551cf5bcb
lryepoch <lryepoch@qq.com>
2021-04-26 16:34:27 +0800

    create 使用nacos作为配置中心

---
+++ springcloud-mall/mall-order/src/main/java/com/order/mall/config/Swagger2Config.java
@@ -0,0 +1,32 @@
+package com.order.mall.config;
+
+
+/**
+ * @author lryepoch
+ * @date 2021/4/26 10:17
+ * @description TODO
+ */
+//@Configuration
+//@EnableSwagger2
+//@EnableKnife4j
+//public class Swagger2Config {
+//
+//    @Bean
+//    public Docket createRestApi(){
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.order.mall.controller"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("admin")
+//                .description("订单后台服务API文档")
+//                .contact("lryepoch")
+//                .version("1.0")
+//                .build();
+//    }
+//}
--- springcloud-mall/mall-product/src/main/java/com/product/mall/controller/ProductController.java
+++ springcloud-mall/mall-product/src/main/java/com/product/mall/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.product.mall.domain.Product;
 import com.product.mall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author lryepoch
  * @date 2021/3/22 11:46
- * @description TODO
+ * @description TODO 商品服务
  */
 @RestController
+@RefreshScope   //在需要动态读取配置的类上添加此注解就可以动态刷新功能。//nacos配置中心
 public class ProductController {
     @Autowired
     private ProductService productService;
 
     @Value("${server.port}")
     private String port;
+
+    @Value("${product.name}")
+    private String name;
 
     //商品信息查询
     @RequestMapping("/product/{pid}")
@@ -29,4 +35,11 @@ public class ProductController {
         return product;
     }
 
+    /**
+    * 引入Nacos作为配置中心测试方法
+    */
+    @GetMapping("/get")
+    public String get(){
+        return name;
+    }
 }
